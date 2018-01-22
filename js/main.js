b3fe9389b7f99fa3759810b48ab86c836b476286
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-22 13:57:26 +0100

    Add files
    Adding files to start the exercice.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*
+CONSIGNE : Avec l'aide de jQuery : 
+
+1/ Nous avons une liste de texture, lorsque l'on clique sur une texture, elle s'applique au fond de la page.
+
+2/ Lorsque l'on passe la souris sur le logo, il se déplace vers le bas.
+
+*/
