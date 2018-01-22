f255d551dfb59e78ba03d2709d66bb62c4bfd956
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-22 14:03:03 +0100

    Fix commentary
    Explicit commentary help.

--- js/main.js
+++ js/main.js
@@ -4,5 +4,10 @@ CONSIGNE : Avec l'aide de jQuery :
 1/ Nous avons une liste de texture, lorsque l'on clique sur une texture, elle s'applique au fond de la page.
 
 2/ Lorsque l'on passe la souris sur le logo, il se déplace vers le bas.
-
+--> fonction Animate() Cf. http://api.jquery.com/animate
 */
+
+
+
+
+
