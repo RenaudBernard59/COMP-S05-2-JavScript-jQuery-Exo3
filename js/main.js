9c1126789aeef44043ea315ec2177d6019412d6d
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-22 16:03:45 +0100

    Alternative code
    Alternative with only one event listener.

--- js/main.js
+++ js/main.js
@@ -6,38 +6,16 @@ CONSIGNE : Avec l'aide de jQuery :
 2/ Lorsque l'on passe la souris sur le logo, il se déplace vers le bas.
 --> fonction Animate() Cf. http://api.jquery.com/animate
 */
-var bc1 = $('.bloc1');
-var bc2 = $('.bloc2');
-var bc3 = $('.bloc3');
-var bc4 = $('.bloc4');
-var bc5 = $('.bloc5');
-var bc6 = $('.bloc6');
+
 var logo = $('#logo');
 
-bc1.on('click', function() {
-	var attribut = bc1.css('background-image');
-	$('body').css('background-image', attribut);
+$(function() {
+	$("#patterns div").on('click', function() {
+		var texture = $(this).css("background-image");
+		$("body").css("background-image", texture);
+	})
 });
-bc2.on('click', function() {
-	var attribut = bc2.css('background-image');
-	$('body').css('background-image', attribut);
-});
-bc3.on('click', function() {
-	var attribut = bc3.css('background-image');
-	$('body').css('background-image', attribut);
-});
-bc4.on('click', function() {
-	var attribut = bc4.css('background-image');
-	$('body').css('background-image', attribut);
-});
-bc5.on('click', function() {
-	var attribut = bc5.css('background-image');
-	$('body').css('background-image', attribut);
-});
-bc6.on('click', function() {
-	var attribut = bc6.css('background-image');
-	$('body').css('background-image', attribut);
-});
+
 
 logo.on('mouseover', function() {
 	logo.animate({
@@ -48,3 +26,5 @@ logo.on('mouseout', function() {
 		fontSize: "100%"
 	}, 100)
 });
+
+
