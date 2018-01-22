c0a3789d4d97a3c4e4ac01691eba15e4db06c0a9
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-22 14:14:53 +0100

    Background-change
    The background-change on click.

--- js/main.js
+++ js/main.js
@@ -6,8 +6,22 @@ CONSIGNE : Avec l'aide de jQuery :
 2/ Lorsque l'on passe la souris sur le logo, il se déplace vers le bas.
 --> fonction Animate() Cf. http://api.jquery.com/animate
 */
+var bc1 = $('.bloc1')
+var bc2 = $('.bloc2')
+var bc3 = $('.bloc3')
+var bc4 = $('.bloc4')
+var bc5 = $('.bloc5')
+var bc6 = $('.bloc6')
 
 
+
+
+
+
+bc1.on('click', function() {
+	var attribut = bc1.css('background-image');
+	$('body').css('background-image', attribut);
+});
 
 
 
